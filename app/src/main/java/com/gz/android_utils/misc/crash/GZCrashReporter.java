a157305e62851619f2db399309112eb3e7e5550f
samuel.zhao.yue <samuel.zhao.yue@live.com>
2016-09-24 22:00:42 +0800

    updae crash reporter

--- app/src/main/java/com/gz/android_utils/misc/crash/GZCrashReporter.java
+++ app/src/main/java/com/gz/android_utils/misc/crash/GZCrashReporter.java
@@ -5,4 +5,23 @@ package com.gz.android_utils.misc.crash;
  * for further issue, please contact: zhaoy.samuel@gmail.com
  */
 public class GZCrashReporter {
+
+    private static GZCrashReporter mInstance;
+
+    public static GZCrashReporter sharedInstance() {
+        if (mInstance == null) {
+            mInstance = new GZCrashReporter();
+        }
+        return mInstance;
+    }
+
+    // Write down exception
+    public void logException(Throwable e) {
+
+    }
+
+    // Upload exception
+    public void reportException() {
+
+    }
 }
