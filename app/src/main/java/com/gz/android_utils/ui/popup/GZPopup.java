7602946b5fe947a5d4c2e67c6837a2d17c880726
samuel.zhao.yue <samuel.zhao.yue@live.com>
2016-09-26 22:11:44 +0800

    support dialog fragment type view

--- app/src/main/java/com/gz/android_utils/ui/popup/GZPopup.java
+++ app/src/main/java/com/gz/android_utils/ui/popup/GZPopup.java
@@ -7,6 +7,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * created by Zhao Yue, at 25/9/16 - 10:01 PM
@@ -23,7 +24,17 @@ public class GZPopup extends DialogFragment {
         return popup;
     }
 
+    public static GZPopup show(View view, Activity activity, String tag) {
+        GZPopup popup = new GZPopup();
+        popup.view = view;
+        popup.show(activity.getFragmentManager(),"tag");
+        /**show the activity*/
+
+        return popup;
+    }
+
     protected int layoutRes;
+    protected View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,7 +46,7 @@ public class GZPopup extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(layoutRes, null))
+        builder.setView( view==null?inflater.inflate(layoutRes, null):view)
                 .setPositiveButton("", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
