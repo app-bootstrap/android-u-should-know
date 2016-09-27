1c26c6bb141d6999bb5f54a0729d5c2d917cc901
samuel.zhao.yue <samuel.zhao.yue@live.com>
2016-09-27 18:36:52 +0800

    update resource manager

---
+++ app/src/main/java/com/gz/android_utils/misc/utils/GZResourceManager.java
@@ -0,0 +1,8 @@
+package com.gz.android_utils.misc.utils;
+
+/**
+ * created by Zhao Yue, at 27/9/16 - 6:22 PM
+ * for further issue, please contact: zhaoy.samuel@gmail.com
+ */
+public class GZResourceManager {
+}
